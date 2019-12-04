dfa3cce82a8a15c050e3f83bc72fedea16814cad
Quinten Ross <qjross76@gmail.com>
2019-12-03 22:32:50 -0500

    better push before I lose this

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ var n = 0
 
 $(document).ready(function() {
 
-$("h1").mouseenter(function(){
-   $(this).css("color" , "gold");
-});
+$("#shsLink").mouseenter(function(){
+ $(this).css({"background-color": "#000000", color: "996600"});
+ });
     
-    ]);
+$("#kmLink").mouseenter(function(){
+ $(this).css({"background-color": "#000000", color: "#EBE343"});
+ });
+    
+   
+$("#crestLink").mouseenter(function(){
+ $(this).css({"background-color": "#0C3726", color: "#EBE343"});
+ });
+
+$("#burnsLink").mouseenter(function(){
+ $(this).css({"background-color": "#06A6E0", color: "#EBE343"});
+ });
+    
+    });
