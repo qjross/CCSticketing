01a8c20372f396acc4c392f124d6574f1ae4ef99
Quinten Ross <qjross76@gmail.com>
2019-12-05 20:06:46 -0500

    graded push

--- script.js
+++ script.js
@@ -2,26 +2,35 @@ var n = 0
 
 $(document).ready(function() {
 
-$("#shsLink").mouseenter(function(){
+$("#shsLink").mouseover(function(){
  $(this).css({"background-color": "#000000", color: "996600"});
  });
     
-$("#kmLink").mouseenter(function(){
+ $("#shsLink").mouseout(function() {
+  $(this).removeAttr("style");
+}); 
+$("#kmLink").mouseover(function(){
  $(this).css({"background-color": "#000000", color: "#EBE343"});
  });
     
+    $("#kmLink").mouseout(function() {
+  $(this).removeAttr("style");
+}); 
    
-$("#crestLink").mouseenter(function(){
+$("#crestLink").mouseover(function(){
  $(this).css({"background-color": "#0C3726", color: "#EBE343"});
  });
+    
+    $("#crestLink").mouseout(function() {
+  $(this).removeAttr("style");
+}); 
 
-$("#burnsLink").mouseenter(function(){
+$("#burnsLink").mouseover(function(){
  $(this).css({"background-color": "#06A6E0", color: "#EBE343"});
  });
     
-    $("#burnsLink").mouseleave(function(){
- $(this).off;
- });
-    
-    
+    $("#burnsLink").mouseout(function() {
+  $(this).removeAttr("style");
+}); 
+ 
     });
